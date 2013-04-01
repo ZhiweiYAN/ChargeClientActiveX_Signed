8c57d0da4f86254bface038dc3b50936a383743e
ZhiweiYAN <jerod.yan@gmail.com>
2013-04-01 12:35:17 +0800

    add purched key

--- ChargeClientActiveX/ChargeClientActiveXidl.h
+++ ChargeClientActiveX/ChargeClientActiveXidl.h
@@ -4,7 +4,7 @@
 
 
  /* File created by MIDL compiler version 6.00.0366 */
-/* at Sat Mar 30 18:33:36 2013
+/* at Sat Mar 30 18:53:19 2013
  */
 /* Compiler settings for .\ChargeClientActiveX.idl:
     Oicf, W1, Zp8, env=Win32 (32b run)
